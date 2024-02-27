d98d54bf8d161597d601eb2d36fc1311b2cd534e
TrungLe <trungle9995@gmail.com>
2024-02-27 04:36:01 -0500

    create Combos.js file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import NavBar from './NavBar';
 import Characters from './Characters';
+import Combos from './Combos'
 import { BrowserRouter as Router,Route,Switch } from 'react-router-dom'
 
 function App() {
@@ -13,6 +14,9 @@ function App() {
           <Switch>
             <Route path="/characters">
               <Characters></Characters>
+            </Route>
+            <Route path="/combos">
+              <Combos></Combos>
             </Route>
           </Switch>
         </div>
---
+++ src/Combos.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Combos(){
+    return(
+        <div className="Combos">
+            <a>COMBOS PAGE</a>
+        </div>
+        
+    )
+}
+
+export default Combos
